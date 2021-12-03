aed247a5060e46f4521b59b893bc2c658b717461
Kanishak08192 <60224870+Kanishak08192@users.noreply.github.com>
2021-12-03 17:07:59 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator,} from 'react-navigation';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import WelcomeScreen from './screens/WelcomeScreen';
-import CartScreen from './screens/CartScreen';
-import { AppDrawerNavigator } from './components/AppDrawerNavigator'
+import HomeScreen from './screens/HomeScreen'
+import SettingScreen from './screens/SettingScreen.js';
+
 
 
 export default function App() {
@@ -12,11 +16,60 @@ export default function App() {
   );
 }
 
+const TabNavigator = createBottomTabNavigator({
+    HomeScreen: {screen: HomeScreen},
+  },
+  {
+    defaultNavigationOptions: ({navigation})=>({
+      tabBarIcon: ()=>{
+        const routeName = navigation.state.routeName;
+        if(routeName === "HomeScreen"){
+          return(
+            <Image
+            source={require("./assets/profile_icon.png")}
+            style={{width:20, height:20}}
+          />
+          )
+
+        }
+      }
+    })
+  }
+);
+
+const AppDrawNavigator = createDrawerNavigator({
+  Home : {
+    screen : TabNavigator
+    },
+  /*Settings : {
+    screen : SettingScreen
+    }
+      SettingsScreen : {
+       SettingScreen
+    } */
+
+  /*SettingScreen : {
+    screen : WelcomeScreen
+    },*/
+
+  /*SettingScreen : {
+    screen : Setting
+    }*/
+  /*SettingScreen : {
+    screen : SettingScreen
+    }*/
+
+  },
+  {
+    //contentComponent:customSidebarMenu
+  },
+  {
+    initialRouteName : 'Home'
+  })
 
 const switchNavigator = createSwitchNavigator({
   WelcomeScreen:{screen: WelcomeScreen},
-  CartScreen:{screen: CartScreen},
-  Drawer:{screen: AppDrawerNavigator}
+  AppDrawNavigator : AppDrawNavigator,
 })
 
 const AppContainer =  createAppContainer(switchNavigator);
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,142 +1,99 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, FlatList,TouchableOpacity } from 'react-native';
-import { ListItem } from 'react-native-elements'
-import firebase from 'firebase';
+import {View,Text, KeyboardAvoidingView,TextInput,StyleSheet,ScrollView,TouchableOpacity, Image} from 'react-native';
+import MyHeader from '../components/MyHeader'
 import db from '../config'
-import MyHeader from '../components/MyHeader';
-
+import firebase from 'firebase'
 
 export default class HomeScreen extends Component{
   
-  constructor(){
-    super()
-    this.state = {
-      requestedItemsList : [],
-      userId : firebase.auth().currentUser.email,
+constructor(){
+    super();
+    this.state={
+      emailId:'',
+      firstName:'',
+      lastName:'',
+      address:'',
+      contact:'',
+      docId:''
     }
-  this.requestRef= null
   }
 
-  getRequestedItemsList =()=>{
-    
-    this.requestRef = db.collection("items")
-    .onSnapshot((snapshot)=>{
-      var requestedItemsList = snapshot.docs.map(document => document.data());
-      this.setState({
-        requestedItemsList : requestedItemsList,
-       
-      });
-    })
-  }
+ getData(){
+  var user = firebase.auth().currentUser;
+  var email= user.email
+ console.log("Hi1"+email)
+ db.collection('users').where('username','==',email).get()
+  .then(snapshot => {
+    console.log("Hi1"+snapshot)
+    snapshot.forEach(doc => {
+       var data = doc.data()
+       console.log("Hi")
+       this.setState({
+         emailId: data.username,
+         firstName:data.first_name,
+         lastName:data.last_name,
+         address:data.address,
+         contact:data.mobile_number,
+         docId:doc.id
+       })
+    });
+  })
 
-  createUniqueId(){
-    return Math.random().toString(36).substring(7);
-  }
-
-
-
-  addRequest =(item_name,item_description)=>{
-    var userId = this.state.userId
-    var randomRequestId = this.createUniqueId()
-
-    db.collection('cart').add({
-        "user_id": userId,
-        "item_name":item_name,
-        "item_description":item_description,
-        
-    })
-  }
-
-  componentDidMount(){
-   
-    this.getRequestedItemsList()
-    
-  }
-
-  componentWillUnmount(){
-    this.requestRef();
-  }
-
-  keyExtractor = (item, index) => index.toString()
-
-  renderItem = ( {item, i} ) =>{
-    
-    return (
-      <ListItem
-        key={i}
-        title={item.item_name}
-        subtitle={item.item_description}
-        titleStyle={{ color: 'black', fontWeight: 'bold' }}
-        
-        rightElement={
-            <TouchableOpacity style={styles.button}
-             onPress = {() => {
-              
-                this.addRequest(item.item_name, item.user_id)
-              //  this.addRequest(item.item_description, item.item_description)
-              //  this.addRequest(item.name, item.description)
-              //  this.addRequest(item.item_name, item.item_description)
-              }}>
-              <Text style={{color:'#ffff'}}>Add to Cart</Text>
-            </TouchableOpacity>
-          }
-        bottomDivider
-      />
-    )
-  }
-
-  render(){
-    return(
-      <View style={{flex:1}}>
-        <MyHeader title="All In One Store" navigation ={this.props.navigation}/>
-        <View style={{flex:1}}>
-          {
-            this.state.requestedItemsList.length === 0
-            ?(
-              <View style={styles.subContainer}>
-                <Text style={{ fontSize: 20}}>List Of All Items in the store</Text>
-              </View>
-            )
-            :(
-              
-              <FlatList
-                  keyExtractor={this.keyExtractor}
-                  
-                  data={this.state.requestedItemsList}
-                  renderItem={this.renderItem}
-
-                
-                  //data={this.state.userId}
-                  //renderItem={this.renderItem}
-
-                  //data={this.state.requestedItemsList}
-                  //renderItem={this.requestedItemsList}
-
-                  //data={this.state.renderItem}
-                  //renderItem={this.renderItem}
-
-              />
-              
-            )
-          }
-        </View>
-      </View>
-    )
-  }
 }
 
+ updateData(){
+
+  db.collection('users').doc(this.state.docId)
+    .update({
+      first_name: this.state.firstName,
+      last_name: this.state.lastName,
+      address:this.state.address,
+      contact:this.state.contact,
+    })
+}
+
+componentDidMount(){
+  console.log("componentDidMount")
+  this.getData()
+}
+
+
+
+   render(){
+        return(
+
+              <View style={{flex:1}} >
+                <MyHeader title="Welcome "/>
+                <View style={{flex:1,width:'100%',alignItems: 'center'}}>
+                  <Text style={styles.formText}> Welcome {this.state.firstName} {this.state.lastName}</Text>          
+                  <Text style={styles.formText}> Contact : {this.state.contact}</Text>
+                  <Text style={styles.formText}> Address : {this.state.address}</Text>
+                  <Text style={styles.formText}> Email : {this.state.emailId}</Text>
+              
+               
+                </View>
+
+              </View>
+
+
+
+
+
+        )
+    }
+}
+
+
 const styles = StyleSheet.create({
-  subContainer:{
-    flex:1,
-    fontSize: 20,
-    justifyContent:'center',
-    alignItems:'center'
-  },
-  button:{
-    width:100,
-    height:30,
-    justifyContent:'center',
-    alignItems:'center',
-    backgroundColor:"#008B8B",
-  }
-})
+   
+    formText:{
+      width:"75%",
+      height:35,
+      alignSelf:'center',
+      marginTop:20,
+      padding:10,
+    },
+   
+
+}
+)
